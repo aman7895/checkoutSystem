7dc760c48c87ef4db5f0c650aae0192a3e4ccfea
aman7895 <aman.middha@gmail.com>
2021-11-07 17:36:55 +0800

    First commit

---
+++ src/main/java/com/aman/checkoutsystem/CheckoutSystemApplication.java
@@ -0,0 +1,13 @@
+package com.aman.checkoutsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aman/checkoutsystem/CheckoutSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.checkoutsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
